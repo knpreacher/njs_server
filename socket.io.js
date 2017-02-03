1dd8bd4d5817d2a36ea77861a4b6d0e35349b736
knp <knpreacher@gmail.com>
2017-02-03 17:38:26 +0300

    asd

--- socket.io.js
+++ socket.io.js
@@ -4,18 +4,30 @@ var express = require('express'),
 console.log('started');
 //app.listen(8080);
 var socket = io.listen(8080);
+var gclient = null;
 socket.on('connection', function (client){
   var ID = (client.id).toString();
   // new client is here!
-  console.log('new con: '+ID);
+  gclient = client;
+  console.log((new Date()) + 'new con: '+ID);
 
   client.send('I\'m '+ID);
-  client.broadcast.send('I\'m here!');
+  client.broadcast.send(ID+' is connected!');
 
   client.on('message', function (message) {
     console.log(message);
   }) ;
-
+  client.on('cmid',function (data){
+    console.log('catched cmid data:'+ data);
+  });
   client.on('disconnect', function () {
+    console.log(ID+' is disconnected');
   });
 });
+socket.on('cmid',function(socket , data){
+  console.log('catched cmid data:'+ data + ' from '+socket);
+});
+socket.on('image',function(image) {
+  console.log('image...');
+  gclient.broadcast.emit('image',image);
+})
